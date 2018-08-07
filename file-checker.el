e3a2509c3f3e4a6ac197f854d609cc1c6fa876b5
jamcha <jamcha.aa@gmail.com>
2018-08-07 18:02:42 +0900

    Add file-checker-both

--- file-checker.el
+++ file-checker.el
@@ -3,16 +3,21 @@
   (when (file-exists-p "~/Documents/")
     (message "aruyo")))
 
-(defun file-checker-cond ()
-  (interactive)
-  (cond ((file-exists-p "~/Naiyo/")
-         nil)
-        (t
-         (message "naiyo"))))
-
 (defun file-checker-summary ()
   (interactive)
   (when (locate-file "SUMMARY.md" '("."))
     (message "aruyo")))
+
+(defun file-checker-both ()
+  (interactive)
+  (cond
+   ((and (locate-file "README.md" '(".")) (locate-file "SUMMARY.md" '(".")))
+     (message "aruyo"))
+   ((locate-file "SUMMARY.md" '("."))
+    (message "Make README.md in the document folder."))
+   ((locate-file "README.md" '("."))
+    (message "Make SUMMARY.md in the document folder."))
+   (t
+    (message "naiyo"))))
 
 (provide 'file-checker)
